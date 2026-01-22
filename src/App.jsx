364ebfdb2533e413ed15270d6c95d04f4343494f
Krish Shah <kshah@trueigtech.com>
2026-01-22 15:40:54 +0530

    Created: Manual form valiation

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    <UserList/>
+    <LoginForm />
     </>
   )
 }
--- src/components/form_manipulation.jsx
+++ src/components/form_manipulation.jsx
@@ -1,13 +1,51 @@
-export const LoginForm = ({ email, setEmail, password, setPassword}) => {
+import {useState} from 'react';
+export const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState({});
+
+    const validateForm = () => {
+        let isValid = true;
+        const newErrors = {};
+        
+        if(!email){
+            newErrors.email = "Email is required"; 
+            isValid = false;
+        }else if(!email.includes('@')){
+            newErrors.email = 'Please enter a valid email';
+            isValid = false;
+        }
+
+        if(!password){
+            newErrors.password = "Password is required";
+            isValid = false;
+        }else if(password.length<6){
+            newErrors.password = 'Please enter a Password of length 6';
+            isValid = false;
+        }
+        setError(newErrors);
+        return isValid;
+    }
+
     const handleSubmit = e =>{
         e.preventDefault();
-        console.log("Login: ", email, password);
+        if(validateForm()){
+            console.log("Login Success", email, password);
+        setError({});
+        }
     }
+
     return(
         <form onSubmit = {handleSubmit}>
-            <input value ={email} onChange={e => setEmail(e.target.value)} placeholder = "Email"/>
-            <input value = {password} onChange={e => setPassword(e.target.value)} placeholder = "password"/>
-            <button>Login</button>
+            <div style={{marginBottom: "10px"}}>
+            <input value ={email} onChange={e => setEmail(e.target.value)} placeholder = "Email"
+            style={{borderColor: error.email? 'red': 'initial'}}/>
+            </div>
+            <div style={{marginBottom: "10px"}}>
+            <input value = {password} onChange={e => setPassword(e.target.value)} placeholder = "password"
+            style={{borderColor: error.password? 'red': 'initial'}}/>
+            </div>
+            <button type="submit">Login</button>
         </form>
     );
 }
